c7407cce7144a8f53cc63e8eb00f9a43641c8f50
muktamittal <38948625+muktamittal@users.noreply.github.com>
2018-05-12 14:18:57 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data.")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinverse(inv)
+    inv
 }
+
+# x = rbind(c(1,3), c(2, 1))
+#create a matrix - 2*2
+# n = makeCacheMatrix(x)
+# n$get()
+#     [,1] [,2]
+#[1,]    1    3
+#[2,]    2    1
+# retrievign inverse the first time
+# cacheSolve(n)
+#     [,1] [,2]
+#[1,] -0.2  0.6
+#[2,]  0.4 -0.2
+
+# Retrieving the cache second time
+# cacheSolve(n)
+#getting cached data.
+#     [,1] [,2]
+#[1,] -0.2  0.6
+#[2,]  0.4 -0.2
+ 
+
+
